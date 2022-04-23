86b4be5116eeb7f77e6d1834b8936a015c5c4c3f
meg-ghana <meghu124@gmail.com>
2022-04-22 20:25:56 -0400

    added project presentations

--- js/scripts.js
+++ js/scripts.js
@@ -51,9 +51,5 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
-    // Activate SimpleLightbox plugin for portfolio items
-    new SimpleLightbox({
-        elements: '#portfolio a.portfolio-box'
-    });
 
 });
